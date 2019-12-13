6b3728657b9470c3d1864f086663fbb2dc38f069
Iuri Pereira <iuricmp@gmail.com>
2019-12-13 09:24:08 -0300

    custom icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div class="glyph fs1">
+            <div class="clearfix bshadow0 pbs">
+              <span class="bm-icon_search"></span>
+              <span class="mls"> bm-icon_search</span>
+            </div>
+          </div>
+          <div class="glyph fs1">
+            <div class="clearfix bshadow0 pbs">
+              <span class="bm-icon_pointer"></span>
+              <span class="mls"> bm-icon_pointer</span>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
